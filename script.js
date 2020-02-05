d4eff3e37cbc3838382802c46c71d36560a399f1
brianyu2 <59840881+brianyu2@users.noreply.github.com>
2020-02-05 03:32:13 -0600

    check2

--- script.js
+++ script.js
@@ -42,28 +42,66 @@ function changeText(x){
             break;
         case 7:
             document.getElementById("runner-description").innerHTML =
-                "Desksol <br> Easy PB: 1:13:36 <br> Legendary PB: 1:32:51 <br> Sol was one of the top runners around 2014 to " +
+                "Desksol <br> Easy PB: (13th) 1:13:36 <br> Legendary PB: (23rd) 1:32:51 <br> Sol was one of the top runners around 2014 to " +
                 "2017, being one of the closest people to a full game world record without obtaining it at only a couple seconds " +
                 "on legendary. He has had the longest standing world record for Halo CE for over four years before Maxlew took it " +
                 "in 2019. Sol also helped with the latest Halo CE Done Segmented.";
             break;
         case 8:
             document.getElementById("runner-description").innerHTML =
-                "Scurty <br> Easy PB: 1:28:16 <br> Legendary PB: 1:29:51 <br> Scurty was a top Halo CE Legendary runner around " +
+                "Scurty <br> Easy PB: (62nd) 1:28:16 <br> Legendary PB: (19th) 1:29:51 <br> Scurty was a top Halo CE Legendary runner around " +
                 "2011-2015, where he with others developed the two major segmented legendary runs, along with getting the " +
                 "Legendary full game record in early 2015.";
             break;
         case 9:
             document.getElementById("runner-description").innerHTML =
-                "Savusukka <br> Easy PB: 1:09:16 <br> Legendary PB: 1:23:06 <br> Savusukka is a top Halo CE runner and glitch " +
+                "Savusukka <br> Easy PB: (4th) 1:09:16 <br> Legendary PB: (10th) 1:23:06 <br> Savusukka is a top Halo CE runner and glitch " +
                 "hunter, getting multiple full game easy world records from 2015 to 2017. He is accredited for many of the tricks " +
                 "that we use today including Belly Skip and Banshee Teleports, along with some other major findings.";
             break;
         case 10:
             document.getElementById("runner-description").innerHTML =
-                "ColdGrunter <br> Easy PB: 1:28:16 <br> Legendary PB: 1:29:51 <br> Scurty was a top Halo CE Legendary runner around " +
-                "2011-2015, where he with others developed the two major segmented legendary runs, along with getting the " +
-                "Legendary full game record in early 2015.";
+                "ColdGrunter <br> Easy PB: (10th) 1:12:55 <br> Legendary PB: (9th) 1:22:21 <br> ColdGrunter was one of the best Halo CE runners from " +
+                "2014-2017, holding full game Easy World Record multiple times, along with being 2nd in Legendary for a long period " +
+                "of time to GarishGoblin. "
+            break;
+        case 11:
+            document.getElementById("runner-description").innerHTML =
+                "GarishGoblin <br> Easy PB: (3rd) 1:09:15 <br> Legendary PB: (1st) 1:09:55 <br> GarishGoblin is the current world record holder " +
+                "for Halo CE Legendary, and is one of the most dominant runners in the game's history. He has ran Halo CE at SGDQ 2017, and " +
+                "has held world record for Legendary for five years.";
+            break;
+        case 12:
+            document.getElementById("runner-description").innerHTML =
+                "SilentWolfJH <br> Easy PB: N/A <br> Legendary PB: (8th) 1:21:00 <br> SilentWolfJH is the current world record holder for Halo CE " +
+                "Legendary on console. He is one of the few console runners and has been one of the major proponents of speedrunning on " +
+                "the Master Chief Collection version of the game.";
+            break;
+        case 13:
+            document.getElementById("runner-description").innerHTML =
+                "Maxlew <br> Easy PB: (2nd) 1:08:00 <br> Legendary PB: (2nd) 1:10:35 <br> Maxlew is one of the best runners of Halo CE currently for " +
+                "both Halo CE Easy and Legendary, holding 2nd place for both categories. He also holds many world records for individual " +
+                "levels and is known for his insane aim in game.";
+            break;
+        case 14:
+            document.getElementById("runner-description").innerHTML =
+                "Chronos <br> Easy PB: (5th) 1:09:27 <br> Legendary PB: (5th) 1:15:14 <br> Chronos (me) is one of the best overall runners of the " +
+                "community, achieving top 5 in both full game categories and top 3 times in every single category on easy and over " +
+                "half of them in legendary. He also holds three individual level world records.";
+            break;
+        case 15:
+            document.getElementById("runner-description").innerHTML =
+                "Cordiaxiz <br> Easy PB: (9th) 1:12:34 <br> Legendary PB: (3rd) 1:14:57 <br> Cordiaxiz is one of the top Legendary runners, holding " +
+                "the 3rd place Legendary time. He is one of the fastest learning runners of the community getting to top 3 in one of the " +
+                "fastest amounts of times in under a couple months. He currently holds many top times, including the Two Betrayals " +
+                "Legendary world record, and the Zero Shot Legendary world record.";
+            break;
+        case 16:
+            document.getElementById("runner-description").innerHTML =
+                "Burnt <br> Easy PB: (6th) 1:10:00 <br> Legendary PB: N/A <br> Burnt is one of the top individual level runners of Halo CE " +
+                "on both Easy and Legendary. He currently holds the world record times for 7 of the 22 possible world records which is " +
+                "the most out of any other runner at this time. He is also known for developing a large number of new strategies and " +
+                "variations which have affected the run greatly today.";
             break;
     }
 }
