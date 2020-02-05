a2eda9e8de494b67b7d428201b3fbb58fa493576
brianyu2 <59840881+brianyu2@users.noreply.github.com>
2020-02-04 21:57:56 -0600

    Initial Upload, still WIP
    TODO: Finish Runners page (half left), Glitches page still unfinished, might edit initial page.

---
+++ script.js
@@ -0,0 +1,69 @@
+function changeText(x){
+    switch (x) {
+        case 1:
+            document.getElementById("runner-description").innerHTML =
+                "Goatrope <br> Easy PB: (36th) 1:21:48 <br> Legendary PB: (7th) 1:20:25 <br> Goatrope is one of the creators of Haloruns.com " +
+                "and has been running since the start of Halo speedrunning with the Going Nowhere Fast competition. " +
+                "He has also taken part in SGDQ 2014, where he completed the game in 1:51:19. He was also the world record " +
+                "holder of Halo CE on Legendary for most of 2014.";
+            break;
+        case 2:
+            document.getElementById("runner-description").innerHTML =
+                "sub_WHISTLE <br> Easy PB: (1st) 1:07:56 <br> Legendary PB: (17th) 1:28:51 <br> sub_WHISTLE, also known " +
+                "as atticus, has been a long time runner of Halo CE since before 2014. He has held both Easy and Legendary " +
+                "world records, with both around late 2014 to 2015, and recently took it from Maxlew at the end of January" +
+                "2020 after returning to the game the past year.";
+            break;
+        case 3:
+            document.getElementById("runner-description").innerHTML =
+                "vetro <br> Easy PB: (22nd) 1:16:46 <br> Legendary PB: (22nd) 1:32:16 <br> Vetro is a former Halo CE runner " +
+                "that has helped create the latest Halo CE Legendary done segmented. He has held many records in the past, " +
+                "including the Easy full game record in late 2014.";
+            break;
+        case 4:
+            document.getElementById("runner-description").innerHTML =
+                "Khan <br> Easy PB: (39th) 1:23:22 <br> Legendary PB: (31st) 1:44:45 <br> Khan is a Halo CE runner that " +
+                "was a major influence in pushing the game to its limits in early 2014. He was able to get the full game " +
+                "legendary world record by doing some very aggressive strategies on very tough sections on levels like" +
+                "Two Betrayals, notorious for its high level of difficulty.";
+            break;
+        case 5:
+            document.getElementById("runner-description").innerHTML =
+                "LePhoque <br> Easy PB: (31st) 1:19:55 <br> Legendary PB: N/A <br> LePhoque is one of the early Halo " +
+                "CE runners, holding one of the first full game world records on HaloRuns with a 1:19:55 as the site's" +
+                "first full game easy world record.";
+            break;
+        case 6:
+            document.getElementById("runner-description").innerHTML =
+                "J2theTon <br> Easy PB: N/A <br> Legendary PB: N/A <br> J2theTon, also known as jumpyard and joshington, " +
+                "is one of the major trick finders in the community. He has helped create and find a ton of the glitches " +
+                "that we use today through all sorts of methods, and was a crucial part in a helping with segmenting runs. " +
+                "He also has held a couple individual level records.";
+            break;
+        case 7:
+            document.getElementById("runner-description").innerHTML =
+                "Desksol <br> Easy PB: 1:13:36 <br> Legendary PB: 1:32:51 <br> Sol was one of the top runners around 2014 to " +
+                "2017, being one of the closest people to a full game world record without obtaining it at only a couple seconds " +
+                "on legendary. He has had the longest standing world record for Halo CE for over four years before Maxlew took it " +
+                "in 2019. Sol also helped with the latest Halo CE Done Segmented.";
+            break;
+        case 8:
+            document.getElementById("runner-description").innerHTML =
+                "Scurty <br> Easy PB: 1:28:16 <br> Legendary PB: 1:29:51 <br> Scurty was a top Halo CE Legendary runner around " +
+                "2011-2015, where he with others developed the two major segmented legendary runs, along with getting the " +
+                "Legendary full game record in early 2015.";
+            break;
+        case 9:
+            document.getElementById("runner-description").innerHTML =
+                "Savusukka <br> Easy PB: 1:09:16 <br> Legendary PB: 1:23:06 <br> Savusukka is a top Halo CE runner and glitch " +
+                "hunter, getting multiple full game easy world records from 2015 to 2017. He is accredited for many of the tricks " +
+                "that we use today including Belly Skip and Banshee Teleports, along with some other major findings.";
+            break;
+        case 10:
+            document.getElementById("runner-description").innerHTML =
+                "ColdGrunter <br> Easy PB: 1:28:16 <br> Legendary PB: 1:29:51 <br> Scurty was a top Halo CE Legendary runner around " +
+                "2011-2015, where he with others developed the two major segmented legendary runs, along with getting the " +
+                "Legendary full game record in early 2015.";
+            break;
+    }
+}
