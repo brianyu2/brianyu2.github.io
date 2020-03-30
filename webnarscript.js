46a18d4e7fa2a879009f1a969890fef05054cd8b
brianyu2 <59840881+brianyu2@users.noreply.github.com>
2020-03-30 04:11:43 -0500

    Completed Web Narrative Project

---
+++ webnarscript.js
@@ -0,0 +1,91 @@
+var i = 0, j = 0, k = 0, l = 0, m = 0, n = 0, o = 0, p = 0;
+var txt1 = 'My name is Edward ██████████ I\'ve decided to start writing today. My psychiatrist said that it would help me relax from both my work and my recent breakup with Rebecca. I\'m not sure what I should write about, so I guess I can say what I did today. █████████████████████████████████████████████████ got my usual coffee early in the morning, drove to work, met with my friend Todd to discuss work. Then we coded for a while before today\'s weekly meeting. We decided to █████████████████████████████████████████ Worked on next week\'s schedule, and went back to coding. Now, I\'m back home in my small city apartment. Really miss the days where Todd and I would run around, ███████████████████████████ especially those with zombies. Maybe next week I\'ll call him and see if he would be down to hang out like we did in college. Speaking of which, the news reported a person was arrested for sleepwalking early in the morning today. Not sure why I remember this, but it feels like a lot of people are feeling sleepy recently so I guess it sticks out to me. Anyways, I\'ll try to keep writing as much as possible, it actually feels really stress relieving. But now, I have a lot of work to do, our project deadline is in two weeks and Terry is a stickler for us to meet our deadlines. I don\'t think it will turn out well though, the tasks seem pretty much impossible to do within the given time. I guess I\'ll look for a job just in case.';
+var txt2 = 'Our group got laid off today. I kind of expected it anyways since the code we wrote did not pass the standards that were required as they were pretty much illogical and especially ████████████████ Terry. I\'ve got interviews though and things are looking up, working at that job was really annoying anyways. Terry apparently wasn\'t feeling very well either, and went back home early that day too. Must have been ███████████████████████████████████████. Lots of people apparently are suffering from ████████████████████ recently, and news stations are reporting that it might be a █████████████████████████. I think people are just overworked in today\'s age. Todd and I are going to go to an arcade today, apparently there\'s also an archery event as well. Would be really cool █████████████████████████████ take some classes in archery since I\'ve always wanted to shoot from that movie we watched last week. Rebecca also texted me for the twentieth time today. She really has issues, I really shouldn\'t read her texts since my psychiatrist said so, but it really did seem like she was in trouble and ███████████████████████████████';
+var txt3 = 'Archery lessons were really fun today! I was top of the class for our long distance target practice and the teacher said I should try out at some competitions. Also got an interview today with a new company that really liked my work. They said that ████████████████████████. Things are really looking up for me! Rebecca and I have started talking again as well, and we\'re on better terms now. Maybe we can get back together some day, but ██████████████████████████, I\' like how things are heading. Todd doesn\'t like the fact that I\'m going to be with her again but, ██████████. The government has apparently issued a safety drill for our city, apparently it\'s a mandatory thing but this is the first time ████████████████. But what scares me is that a couple tanks and soldiers are apparently being deployed here. They say it\'s  for next week\'s air show but what kind of air show features tanks? Also just noticed an obituary post in the newspaper today for Terry, apparently he ████████████████████. Serves him right, never liked the guy anyways.';
+var txt4 = 'What is going on? I heard loud █████████████ coming from the city █████████████ a couple miles away, and lots of people were ██████████████████, Todd, and Rebecca just so happened to be over at my place for a mini reunion so we\'re safe but that doesn\'t mean █████████████ our plan ███████████  to get out of the city and drive out. Todd has a heavy duty truck so we\'re planning on offroading it and escaping while we can. No need to worry about laws, it\'s an emergency after all. I hope everyone else is █████████████. █████████████████████ edit, I saw what looked like to be Terry, he was █████████████████, and I shot █████████████ arrow, ██████████████████ I don\'t know what is going on any more██ it\'s as if hell is on earth right now and ████████████████████████████████████████ ████████████████████████████████████████████ is ████████ ██████████████ and ███████████████ ██████████████ █████████ █████████████ save ██████████ ████████████the only ███ ████████ spread by bite  █████████████████ key ████████████████████.';
+var txt5 = 'The outbreak of living dead has costed ██████████ ███████ were taken into a society so we can help find a cure to this zombie virus █████ ████████████ █████ █████. Todd and I were proficient at coding and had some other useful technical skills, so working with a couple of the others who were doctors and █████████████████████ find a cure through genome sequencing. Rebecca is still in shock though from last month when I shot Terry, no the zombie version of him,  ████████████████████ she can get better soon. Our group has decided that me keeping a log of how things are going is important for our society, and have decided that I should keep writing in this online document. Issue is electricity, but we have managed to get a generator stabilized here. I\'m really amazed that my other laptop was able to stay alive as long as it has, same with my phone. Turns out the makeshift battery we designed worked really well. Sadly the internet doesn\'t work anymore. I never realized how quiet things feel without it. It also feels really weird that I can\'t tell what is going elsewhere in the world. Last I heard people were trying to escape  ████████████████████ and  ████████████████████';
+var txt6 = 'Rebecca passed away. I feel so alone. ██████████ █████████████████ ███████ ██████████ ███████ ██████████ █████████████████ ███████ ██████████ ███████ ██████████ █████████████████ ███████We must keep going forwards. It\'s what she would want. Our food supplies are not well and we need to venture on and reach colder weather. The zombies tend to move slower in the cold weather so  ████████████████████. Our group size has been cut in half but we\'re getting so close to it. Things are going slower though, since more of us have to go out to get food.  █████████████████████ hope Todd will do alright, he\'s on hunting duty today. Thank god we had archery classes before this outbreak happened. Without it I would have no idea how we would survive out in the wilderness like this. We were also lucky I guess that most of society has bonded together from this experience, very different than those in the movies where society completely breaks down. There are a few oddballs here and there but most people are still sticking together, for power with numbers and are working towards solving this terrible problem.';
+var txt7 = 'We got the cure! Todd was the recipient of it and I can\'t believe it! He lived the virus! We can do this. I\'ll type out what I can here: ██████████ █████████████████ ███████ ██████████ ███████ ██████████ █████████████████ ███████ ██████████ ███████ ██████████ █████████████████ █████████████████ █████████████████ ███████ ██████████ ███████ ██████████ █████████████████ ███████ ██████████ ███████ ██████████ █████████████████ ███████ ██████████ █████████████████ ███████ ██████████ ███████ ██████████ █████████████████ ███████ ██████████ ███████ ██████████ █████████████████ ███████ ██████████ █████████████████ ███████ ██████████ ███████ ██████████ █████████████████ ███████ ██████████ ███████ ██████████ █████████████████ █████████████████ █████████████████ ███████ ██████████ ███████ ██████████ █████████████████ ███████ ██████████ ███████ ██████████ █████████████████ ███████ ██████████ █████████████████ ███████ ██████████ ███████ ██████████ █████████████████ ███████ ██████████ ███████ ██████████ █████████████████ █████████████████ █████████████████ ███████ ██████████ ███████ ██████████ █████████████████ ███████ ██████████ ███████ ██████████ █████████████████ ███████';
+var txt8 = 'There isn\'t much time, we shouldn\'t have announced the cure. It was stupid. We\'re screwed. Raiders are coming in and plan on destroying everything. If you\'re reading this please use this as a guide. The code is here: ██████████████████. The eighteen digits hold █████████ cure to the virus. You can only access it with the correct combination. In case these files get damaged from the hard drive. I have tried to find another way to hide it here for safekeeping if you can decrypt it. The answer lies within here. This is the end of my story. I hope you can find the key to our future. Goodbye, I will miss you, Rebecca, Todd, my family, friends and the rest. May we meet again in another peaceful place in another life. My name is ██████ Johnson, and this is my final entry.';
+var speed = 10; /* The speed/duration of the effect in milliseconds */
+
+function clear() {
+    document.getElementById("text1").innerHTML = '';
+}
+
+function typeWriter1() {
+    if (i < txt1.length) {
+        document.getElementById("text1").innerHTML += txt1.charAt(i);
+        i++;
+        setTimeout(typeWriter1, speed);
+    }
+}
+
+
+function typeWriter2() {
+    if (j < txt2.length) {
+        document.getElementById("text2").innerHTML += txt2.charAt(j);
+        j++;
+        setTimeout(typeWriter2, speed);
+    }
+}
+
+function typeWriter3() {
+    if (k < txt3.length) {
+        document.getElementById("text3").innerHTML += txt3.charAt(k);
+        k++;
+        setTimeout(typeWriter3, speed);
+    }
+}
+
+function typeWriter4() {
+    if (l < txt4.length) {
+        document.getElementById("text4").innerHTML += txt4.charAt(l);
+        l++;
+        setTimeout(typeWriter4, speed);
+    }
+}
+
+function typeWriter5() {
+    if (m < txt5.length) {
+        document.getElementById("text5").innerHTML += txt5.charAt(m);
+        m++;
+        setTimeout(typeWriter5, speed);
+    }
+}
+
+function typeWriter6() {
+    if (n < txt6.length) {
+        document.getElementById("text6").innerHTML += txt6.charAt(n);
+        n++;
+        setTimeout(typeWriter6, speed);
+    }
+}
+
+function typeWriter7() {
+    if (o < txt7.length) {
+        document.getElementById("text7").innerHTML += txt7.charAt(o);
+        o++;
+        setTimeout(typeWriter7, speed);
+    }
+}
+
+function typeWriter8() {
+    if (p < txt8.length) {
+        document.getElementById("text8").innerHTML += txt8.charAt(p);
+        p++;
+        setTimeout(typeWriter8, speed);
+    }
+}
+
+function checkVals() {
+    var fname = document.getElementById("fname").value;
+    var lname = document.getElementById("lname").value;
+    var code = document.getElementById("code").value;
+    if (fname === "edward" && lname === "johnson" && code === "116376598259352367") {
+        window.open(
+            "end.html", "_blank");
+    } else {
+        alert("Invalid Data.");
+    }
+}
